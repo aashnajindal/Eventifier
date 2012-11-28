8a4c1c9b5c9ae671f40040fbca12c2a2c5da2a6c
Aashna Jindal <AashnaJindal@Aashnas-MacBook-Pro.local>
2012-11-28 03:45:50 -0500

    Storyboard done. Yayyyy. With tabs bitches. Let me exclamate

--- mvc.py
+++ mvc.py
@@ -2,36 +2,118 @@ from Tkinter import *
 
 def init():
 	splashScreen()
-	#tabFlags = dict()
-	#tabFlags["home"] = True
-	#tabFlags["venuePage"] = False
-	#tabFlags["dateTimePage"] = False
-	#tabFlags["summaryPage"] = False
-	#canvas.data.tabFlags = tabFlags
+	canvas.data.venueFlag = False
+	canvas.data.dateTimeFlag = False
+	canvas.data.overviewFlag = False
 	canvas.data.homeIcon = PhotoImage(file="HomeIcon.gif")
 	canvas.data.eH_1 = Entry(canvas.data.root, width = 30)
 	canvas.data.eH_2 = Entry(canvas.data.root, width = 30)
-	canvas.data.bH_1 = Button(canvas.data.root,text = "GO",command = timeToGo1)
+	canvas.data.bH_1 = Button(canvas.data.root,text = "Go",command = timeToGo1)
 	canvas.data.bH_2 = Button(canvas.data.root, 
 	image = canvas.data.homeIcon, command = homeScreen)
 	canvas.data.eV_1 = Entry(canvas.data.root, width = 30)
 	canvas.data.eV_2 = Entry(canvas.data.root, width = 30)
 	canvas.data.bV_1 = Button(canvas.data.root, text = "Go", 
-	command = dateTimeScreen)
+	command = timeToGo2)
 	canvas.data.bV_2 = Button(canvas.data.root, text = "Search", 
 	command = venueSearch)
+	canvas.data.eD_1 = Entry(canvas.data.root, width = 30)
+	canvas.data.eD_2 = Entry(canvas.data.root, width = 30)
+	canvas.data.eD_3 = Entry(canvas.data.root, width = 30)
+	canvas.data.bD_1 = Button(canvas.data.root, text = "Go", 
+	command = timeToGo3)
+	canvas.data.bT_1 = Button(canvas.data.root, text = "Venue",
+	command = goToVenue)
+	canvas.data.bT_2 = Button(canvas.data.root, text = "Date/Time",
+	command = goToDateTime)
+	canvas.data.bT_3 = Button(canvas.data.root, text = "Overview",
+	command = goToOverview)
 	canvas.data.allWidgets = [canvas.data.eH_1, canvas.data.eH_2,
 	canvas.data.bH_1, canvas.data.eV_1, canvas.data.eV_2, canvas.data.bV_1,
-	canvas.data.bV_2]
+	canvas.data.bV_2, canvas.data.eD_1, canvas.data.eD_2, canvas.data.eD_3,
+	canvas.data.bD_1]
 
 def venueSearch(): pass
 
-def dateTimeScreen(): pass
+def dateTimeScreen():
+	deleteAll()
+	canvasWidth = canvas.data.canvasWidth
+	canvasHeight = canvas.data.canvasHeight
+	canvas.create_rectangle(3, 3, canvasWidth, canvasHeight, fill = "#fef6e4")
+	canvas.create_text(canvasWidth/2,30, text = canvas.data.eventName, 
+	font = "Helvetica 35 bold underline", fill = "#00CED1")
+	canvas.create_text(canvasWidth/2, 100, text = "DATE & TIME",
+	font = "Helvetica 30 bold underline" )
+	canvas.create_text(113, 200, text = "Date: ", 
+	font = "Helvetica 25 bold")
+	canvas.create_text(82, 300, text = "Start Time: ", 
+	font = "Helvetica 25 bold")
+	canvas.create_text(85, 400, text = "End Time: ", 
+	font = "Helvetica 25 bold")
+	canvas.data.eD_1.place(x = 170, y = 190)
+	canvas.data.eD_2.place(x = 170, y = 290)
+	canvas.data.eD_3.place(x = 170, y = 390)
+	canvas.data.bD_1.place(x = 270, y = 470)
+	canvas.data.bT_1.place(x = 10, y = 550)
+	canvas.data.bT_2.place(x = 80, y = 550)
+	canvas.data.bT_3.place(x = 180, y = 550)
+
+
+def goToVenue():
+	if(canvas.data.venueFlag == True): timeToGo1()
+
+def goToDateTime(): 
+	if(canvas.data.venueFlag == True): timeToGo2()
+	if(canvas.data.dateTimeFlag == True): timeToGo2()
+
+def goToOverview():
+	if(canvas.data.dateTimeFlag == True): timeToGo3()
+	if(canvas.data.overviewFlag == True): timeToGo3()
 
 def timeToGo1():
 	canvas.data.eventName = canvas.data.eH_1.get()
 	canvas.data.eventType = canvas.data.eH_2.get()
+	canvas.data.venueFlag = True
 	venueScreen1()
+
+def timeToGo2():
+	canvas.data.eventVenue = canvas.data.eV_1.get()
+	canvas.data.dateTimeFlag = True
+	dateTimeScreen()
+
+def timeToGo3():
+	canvas.data.eventDate = canvas.data.eD_1.get()
+	canvas.data.eventStartTime = canvas.data.eD_2.get()
+	canvas.data.eventEndTime = canvas.data.eD_3.get()
+	canvas.data.overviewFlag = True
+	summaryScreen()
+
+def summaryScreen():
+	deleteAll()
+	canvasWidth = canvas.data.canvasWidth
+	canvasHeight = canvas.data.canvasHeight
+	venueText = "Venue: " + canvas.data.eventVenue
+	dateText = "Date: " + canvas.data.eventDate
+	if(canvas.data.eventStartTime==""): timeText = "Time: "
+	elif(canvas.data.eventStartTime!="" and canvas.data.eventEndTime==""):
+		timeText = "Time: " + canvas.data.eventStartTime
+	else:
+		timeText = "Time: " + canvas.data.eventStartTime + " to " + canvas.data.eventEndTime
+	canvas.create_rectangle(3, 3, canvasWidth, canvasHeight, fill = "#fef6e4")
+	canvas.create_text(canvasWidth/2,30, text = canvas.data.eventName, 
+	font = "Helvetica 35 bold underline", fill = "#00CED1")
+	canvas.create_text(canvasWidth/2, 100, text = "OVERVIEW",
+	font = "Helvetica 30 bold underline")
+	canvas.create_text(50, 200, text = venueText, 
+	font = "Helvetica 20 bold", anchor = NW)
+	canvas.create_text(50, 300, text = dateText, 
+	font = "Helvetica 20 bold", anchor = NW)
+	canvas.create_text(50, 400, text = timeText, 
+	font = "Helvetica 20 bold", anchor = NW)
+	canvas.data.bT_1.place(x = 10, y = 550)
+	canvas.data.bT_2.place(x = 80, y = 550)
+	canvas.data.bT_3.place(x = 180, y = 550)
+
 
 def venueScreen1():
 	deleteAll()
@@ -52,6 +134,9 @@ def venueScreen1():
 	canvas.data.eV_2.place(x = 170, y = 420)
 	canvas.data.bV_1.place(x = 375, y = 250)
 	canvas.data.bV_2.place(x = 350, y = 450)
+	canvas.data.bT_1.place(x = 10, y = 550)
+	canvas.data.bT_2.place(x = 80, y = 550)
+	canvas.data.bT_3.place(x = 180, y = 550)
 
 def deleteAll():
 	canvas.delete(ALL)
@@ -85,12 +170,12 @@ def homeScreen():
 	canvas.data.eH_1.insert(0, "Plan A")
 	canvas.data.eH_2.insert(0, "Party")
 
-
 def run():
 	global canvas
 	root = Tk()
 	canvasWidth = 600
 	canvasHeight = 600
+	root.title("Eventifier")
 	canvas = Canvas(root, width = canvasWidth, height = canvasHeight)
 	canvas.pack()
 	root.resizable(width=0, height=0)
