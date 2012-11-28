17806a42f915d08951dd433100673b4d727db0e0
Aashna Jindal <AashnaJindal@Aashnas-MacBook-Pro.local>
2012-11-28 01:07:27 -0500

    venueScreen1 donewith textboxes and buttons and everything

--- mvc.py
+++ mvc.py
@@ -1,22 +1,62 @@
 from Tkinter import *
 
-
-def mousePressed(event):pass
-	#redrawAll()
-
-def keyPressed(event): pass
-	#redrawAll()
-
 def init():
 	splashScreen()
+	#tabFlags = dict()
+	#tabFlags["home"] = True
+	#tabFlags["venuePage"] = False
+	#tabFlags["dateTimePage"] = False
+	#tabFlags["summaryPage"] = False
+	#canvas.data.tabFlags = tabFlags
+	canvas.data.homeIcon = PhotoImage(file="HomeIcon.gif")
 	canvas.data.eH_1 = Entry(canvas.data.root, width = 30)
 	canvas.data.eH_2 = Entry(canvas.data.root, width = 30)
-	canvas.data.bH_1 = Button(canvas.data.root, text = "GO", command = timeToGo)
+	canvas.data.bH_1 = Button(canvas.data.root,text = "GO",command = timeToGo1)
+	canvas.data.bH_2 = Button(canvas.data.root, 
+	image = canvas.data.homeIcon, command = homeScreen)
+	canvas.data.eV_1 = Entry(canvas.data.root, width = 30)
+	canvas.data.eV_2 = Entry(canvas.data.root, width = 30)
+	canvas.data.bV_1 = Button(canvas.data.root, text = "Go", 
+	command = dateTimeScreen)
+	canvas.data.bV_2 = Button(canvas.data.root, text = "Search", 
+	command = venueSearch)
+	canvas.data.allWidgets = [canvas.data.eH_1, canvas.data.eH_2,
+	canvas.data.bH_1, canvas.data.eV_1, canvas.data.eV_2, canvas.data.bV_1,
+	canvas.data.bV_2]
 
-def timeToGo():
+def venueSearch(): pass
+
+def dateTimeScreen(): pass
+
+def timeToGo1():
 	canvas.data.eventName = canvas.data.eH_1.get()
 	canvas.data.eventType = canvas.data.eH_2.get()
-	print canvas.data.eventName, canvas.data.eventType
+	venueScreen1()
+
+def venueScreen1():
+	deleteAll()
+	canvasWidth = canvas.data.canvasWidth
+	canvasHeight = canvas.data.canvasHeight
+	canvas.create_rectangle(3, 3, canvasWidth, canvasHeight, fill = "#fef6e4")
+	canvas.create_text(canvasWidth/2,30, text = canvas.data.eventName, 
+	font = "Helvetica 35 bold underline", fill = "#00CED1")
+	canvas.create_text(canvasWidth/2, 100, text = "VENUE",
+	font = "Helvetica 30 bold underline" )
+	canvas.create_text(canvasWidth/2, 200, 
+	text = "Please enter your own venue", font = "Helvetica 15 bold" )
+	canvas.create_text(canvasWidth/2, 320, text = "OR", 
+	font = "Helvetica 25 bold" )
+	canvas.create_text(canvasWidth/2, 400, 
+	text = "Please enter city or zipcode", font = "Helvetica 15 bold")
+	canvas.data.eV_1.place(x = 170, y = 220)
+	canvas.data.eV_2.place(x = 170, y = 420)
+	canvas.data.bV_1.place(x = 375, y = 250)
+	canvas.data.bV_2.place(x = 350, y = 450)
+
+def deleteAll():
+	canvas.delete(ALL)
+	for i in xrange(len(canvas.data.allWidgets)):
+		canvas.data.allWidgets[i].place_forget()
 
 def splashScreen():
 	canvasWidth = canvas.data.canvasWidth
@@ -25,21 +65,18 @@ def splashScreen():
 	canvas.create_text(canvasWidth/2, canvasHeight/2, fill = "purple",
 	text = "EVENTIFIER", font = "Helvetica 80 bold")
 
-def timerFired():
-	redrawAll()
-	#delay = 250
-	#canvas.after(delay, timerFired)
-
-def redrawAll():
+def homeScreen():
+	deleteAll()
 	canvasWidth = canvas.data.canvasWidth
 	canvasHeight = canvas.data.canvasHeight
-	canvas.create_rectangle(0, 0, canvasWidth, canvasHeight, fill = "grey")
-	homeIcon = PhotoImage(file="HomeIcon.gif")
-	canvas.data.eH_1.place(x = 150, y = 180)
-	canvas.data.eH_2.place(x = 150, y = 330)
-	canvas.data.bH_1.place(x = 420, y = 330)
-	canvas.create_image(500, 20 , image = homeIcon, anchor = NW)
-	canvas.data.t = canvas.create_text(canvasWidth/2, 150, fill = "black",
+	canvas.create_rectangle(3, 3, canvasWidth, canvasHeight, fill = "#fef6e4")
+	canvas.data.eH_1.place(x = 165, y = 180)
+	canvas.data.eH_2.place(x = 165, y = 330)
+	canvas.data.bH_1.place(x = 370, y = 360)
+	canvas.data.bH_2.place(x = 530, y = 20)
+	canvas.data.eH_1.delete(0 , len(canvas.data.eH_1.get()))
+	canvas.data.eH_2.delete(0 , len(canvas.data.eH_2.get()))
+	canvas.create_text(canvasWidth/2, 150, fill = "black",
 	text = "Name your event.", font = "Helvetica 24 bold")
 	canvas.create_text(canvasWidth/2, 300, fill = "black",
 	text = "What event do you want to plan?", font = "Helvetica 24 bold")
@@ -64,10 +101,8 @@ def run():
 	canvas.data.canvasHeight = canvasHeight
 	canvas.data.root = root
 	init()
-	root.bind("<Button-1>", mousePressed)
-	root.bind("<Key>", keyPressed)
 	delay = 1500
-	canvas.after(delay, timerFired)
+	canvas.after(delay, homeScreen)
 	root.mainloop()
 
 run()
