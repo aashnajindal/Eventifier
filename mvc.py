87bebf9086b1af23ccef1d7bacdc3d6493aaa8cc
Aashna Jindal <AashnaJindal@Aashnas-MacBook-Pro.local>
2012-11-27 20:25:59 -0500

    Textboxes, picrure, kind ofish

--- mvc.py
+++ mvc.py
@@ -1,8 +1,12 @@
+#from PIL import Image
 from Tkinter import *
 
-def mousePressed(event): pass
+
+def mousePressed(event):pass
+	#redrawAll()
 
 def keyPressed(event): pass
+	#redrawAll()
 
 def init():
 	splashScreen()
@@ -14,9 +18,30 @@ def splashScreen():
 	canvas.create_text(canvasWidth/2, canvasHeight/2, fill = "purple",
 	text = "EVENTIFIER", font = "Helvetica 80 bold")
 
-def timerFired():pass
+def timerFired():
+	redrawAll()
+	#delay = 250
+	#canvas.after(delay, timerFired)
 
-def redrawAll():pass
+def redrawAll():
+	canvasWidth = canvas.data.canvasWidth
+	canvasHeight = canvas.data.canvasHeight
+	canvas.create_rectangle(0, 0, canvasWidth, canvasHeight, fill = "grey")
+	homeIcon = PhotoImage(file="HomeIcon.gif")
+	canvas.data.e1 = Entry(canvas.data.root, width = 30)
+	canvas.data.e2 = Entry(canvas.data.root, width = 30)
+	canvas.data.e1.place(x = 150, y = 180)
+	canvas.data.e2.place(x = 150, y = 330)
+	canvas.create_image(500, 20 , image = homeIcon, anchor = NW)
+	canvas.data.t = canvas.create_text(canvasWidth/2, 150, fill = "black",
+	text = "Name your event.", font = "Helvetica 24 bold")
+	canvas.create_text(canvasWidth/2, 300, fill = "black",
+	text = "What event do you want to plan?", font = "Helvetica 24 bold")
+	canvas.create_text(canvasWidth/2, 50, fill = "black", 
+	text = "HOME", font = "Helvetica 36 bold underline")
+
+
+
 
 def run():
 	global canvas
@@ -31,10 +56,12 @@ def run():
 	canvas.data = Struct()
 	canvas.data.canvasWidth = canvasWidth
 	canvas.data.canvasHeight = canvasHeight
+	canvas.data.root = root
 	init()
 	root.bind("<Button-1>", mousePressed)
 	root.bind("<Key>", keyPressed)
-	timerFired()
+	delay = 1500
+	canvas.after(delay, timerFired)
 	root.mainloop()
 
 run()
